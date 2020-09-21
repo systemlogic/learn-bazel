5f442adef5b071b1bb93e6d833a4a58793fb7e73
GIT <systemlogic@users.noreply.github.com>
2020-09-22 01:02:05 +0530

    check for change (#6)
    * check for change
    
    * build targets
    
    * cc
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update bazel
    
    * Update bazel
    
    * Update bazel
    
    * Update bazel
    
    * Update BUILD
    
    * Update bazel
    
    * Update .travis.yml

--- cc/clientServer/client.cc
+++ cc/clientServer/client.cc
@@ -16,6 +16,7 @@
  *
  */
 
+
 #include <iostream>
 #include <memory>
 #include <string>
--- java/com/systemlogic/sample/Hello.java
+++ java/com/systemlogic/sample/Hello.java
@@ -2,6 +2,7 @@ package com.systemlogic.sample;
 
 import java.lang.String;
 
+
 public class Hello {
   public static String sayHello(String name) {
     return String.format("Hello %s, greetings sent from java src code", name);
